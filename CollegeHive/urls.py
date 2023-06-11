1c78c93421dfa4eb1de16968566156d8ff2e8016
unknown <Om Satturwar>
2023-06-11 20:41:26 +0530

    Second Iteration

--- CollegeHive/urls.py
+++ CollegeHive/urls.py
@@ -7,5 +7,15 @@ urlpatterns = [
     path("properties/",views.properties,name='properties'),
     path("login/",views.login,name='login'),
     path("register/",views.register,name='register'),
-    path("properties/bharatmata-society",views.bharatmata,name='bharatmata-society')
+    path("properties/bharatmata-society",views.bharatmata,name='bharatmata-society'),
+    path("properties/garva-girls-hostel",views.garva,name='garva-girls-hostel'),
+    path("properties/guru-om-hostel",views.guruom,name='guru-om-hostel'),
+    path("properties/gurukunj-hostel",views.gurukunj,name='gurukunj-hostel'),
+    path("properties/harshraj-residency",views.harshraj_residency,name='harshraj-residency'),
+    path("properties/mohan-villa",views.mohanvilla,name='mohan-villa'),
+    path("properties/sai-glamour-residency",views.sai_glamour,name='sai-glamour-residency'),
+    path("properties/saptashrungi-society",views.saptashrungi,name='saptashrungi-society'),
+    path("properties/shree-ganesh-pg",views.shree_ganesh_pg,name='shree-ganesh-pg'),
+    path("properties/shree-pg-services",views.shree_pg_services,name='shree-pg-services'),
+    path("properties/subhadra-hostel",views.subhadra_hostel,name='subhadra-hostel')
 ]
--- CollegeHive/views.py
+++ CollegeHive/views.py
@@ -15,3 +15,34 @@ def register(request):
 
 def bharatmata(request):
     return render(request,'bharatmata-society.html')
+
+def garva(request):
+    return render(request,'garva-girls-hostel.html')
+
+def guruom(request):
+    return render(request,'guru-om-hostel.html')
+
+def gurukunj(request):
+    return render(request,'gurukunj-hostel.html')
+
+def harshraj_residency(request):
+    return render(request,'harshraj-residency.html')
+
+def mohanvilla(request):
+    return render(request,'mohan-villa.html')
+
+def sai_glamour(request):
+    return render(request,'sai-glamour-residency.html')
+
+def saptashrungi(request):
+    return render(request,'saptashrungi-society.html')
+
+def shree_ganesh_pg(request):
+    return render(request,'shree-ganesh-pg.html')
+
+def shree_pg_services(request):
+    return render(request,'shree-pg-services.html')
+
+def subhadra_hostel(request):
+    return render(request,'subhadra-hostel.html')
+
